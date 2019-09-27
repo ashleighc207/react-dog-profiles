4bc84ed30dc6a284d19c882b1341b72436bf1d14
Ashleigh Christopher <ashleighc207@gmail.com>
2019-09-27 10:51:54 -0400

    add in facts for dogs

--- src/Components/DogContent.js
+++ src/Components/DogContent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import dogs from "../Models/dogs.js";
+import "./DogContent.css";
 
 class DogContent extends Component {
   render() {
@@ -7,7 +8,7 @@ class DogContent extends Component {
       return d.name === this.props.match.params.id;
     });
     return (
-      <section className="DogContent--profile">
+      <section className="DogContent">
         <div className="DogContent--img_container">
           <img
             className="DogContent--img"
@@ -16,6 +17,11 @@ class DogContent extends Component {
           />
         </div>
         <p className="DogContent--name">{selectedDog[0].name}</p>
+        <ul className="DogContent--fact_list">
+          {selectedDog[0].facts.map(f => {
+            return <li className="DogContent--fact">{f}</li>;
+          })}
+        </ul>
       </section>
     );
   }
--- src/Models/dogs.js
+++ src/Models/dogs.js
@@ -2,32 +2,62 @@ const dogs = [
   {
     id: "1",
     name: "Bucky",
-    img: "/bucky.jpeg"
+    img: "/bucky.jpeg",
+    facts: [
+      "loves playing dress up",
+      "will give you side eye",
+      "hates other dogs"
+    ]
   },
   {
     id: "2",
     name: "Almond",
-    img: "/almond.jpeg"
+    img: "/almond.jpeg",
+    facts: [
+      "loves the water",
+      "always full of energy",
+      "sometimes runs into walls"
+    ]
   },
   {
     id: "3",
     name: "Lily",
-    img: "/lily.jpeg"
+    img: "/lily.jpeg",
+    facts: [
+      "definitely a princess",
+      "randomly brings home flowers",
+      "loves shopping for new toys"
+    ]
   },
   {
     id: "4",
     name: "Snuggles",
-    img: "/snuggles.jpeg"
+    img: "/snuggles.jpeg",
+    facts: [
+      "naps literally all the time",
+      "will sleep anywhere he fits",
+      "sometimes sleeps sitting up"
+    ]
   },
   {
     id: "5",
     name: "Jack",
-    img: "/jack.jpeg"
+    img: "/jack.jpeg",
+    facts: [
+      "loves going to the park",
+      "always looks like he is smiling",
+      "has a super fast trot"
+    ]
   },
   {
     id: "6",
     name: "Super Dog",
-    img: "/super-dog.jpeg"
+    img: "/super-dog.jpeg",
+    facts: [
+      "saves the world daily",
+      "has secret identity",
+      "does minor reporting on the side"
+    ]
   }
 ];
 
