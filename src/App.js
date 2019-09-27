bac20e2b1163d6e8d0fad4c37a70aa5cf4a0de47
Ashleigh Christopher <ashleighc207@gmail.com>
2019-09-27 11:11:16 -0400

    adjust basename

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router basename={`${process.env.PUBLIC_URL}/react-dog-profiles/`}>
         <Nav />
         <Switch>
           <Route exact path="/" component={Profiles} />
